e473b69e4c962b67efcf38f6c5e5ea01b18c4714
Hammad ul haq <professorcoding123@gmail.com>
2024-03-05 18:15:17 +0500

    Converted the admin panel for nykn

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<Splash />} /> */}
+        <Route path="/" element={<Splash />} />
 
         {loginRoutes?.map((route, key) => {
           return (
--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -6,24 +6,25 @@ const Splash = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    setTimeout(() => {
-      const icon = document.getElementById("splash-animate-icon");
-      icon.classList.add("rotate-45");
-    }, 400);
-    setTimeout(() => {
-      const icon = document.getElementById("splash-animate-icon");
-      icon.classList.add("translate-x-[60vw]");
-    }, 800);
+    // setTimeout(() => {
+    //   const icon = document.getElementById("splash-animate-icon");
+    //   icon.classList.add("rotate-45");
+    // }, 400);
+    // setTimeout(() => {
+    //   const icon = document.getElementById("splash-animate-icon");
+    //   icon.classList.add("translate-x-[60vw]");
+    // }, 800);
 
-    setTimeout(() => {
-      const icon = document.getElementById("splash-animate-logo");
-      icon.classList.remove("translate-x-[100vw]");
-      icon.classList.add("translate-x-1/2");
-    }, 1200);
+    // setTimeout(() => {
+    //   const icon = document.getElementById("splash-animate-logo");
+    //   icon.classList.remove("translate-x-[100vw]");
+    //   icon.classList.add("translate-x-1/2");
+    // }, 1200);
 
-    setTimeout(() => {
-      navigate("/login");
-    }, 1400);
+    // setTimeout(() => {
+    //   navigate("/login");
+    // }, 1400);
+    navigate("/login");
   }, []);
 
   const { palette } = useContext(GlobalContext);
@@ -35,7 +36,7 @@ const Splash = () => {
         background: palette?.background,
       }}
     >
-      <img
+      {/* <img
         src="logo.svg"
         className="fixed top-[45%] left-0 h-16 translate-x-[100vw] z-50"
         id="splash-animate-logo"
@@ -44,7 +45,7 @@ const Splash = () => {
         src="/rentalbay-icon.png"
         id="splash-animate-icon"
         className="transition-all fixed top-[45%] left-[45%] duration-200"
-      />
+      /> */}
     </div>
   );
 };
